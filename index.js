63ea957cc87d78568d840806dfe8db200704c98f
dweet <dweet.parikh16@gmail.com>
2026-04-08 14:15:26 +0530

    added backend

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.post("/sum", function(req, res) {
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+
+    res.json({
+        ans: a + b
+    });
+});
+
+app.listen(3000);
